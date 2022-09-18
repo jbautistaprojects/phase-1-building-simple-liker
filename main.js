bbdfe5e7b2ca9ef57a3f87af155ab58f2251cb63
Justina Mandaro <bautistajustina7@gmail.com>
2022-09-18 13:35:48 -0700

    created simple like button with mimic server reaction

--- main.js
+++ main.js
@@ -2,14 +2,44 @@
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
-// Your JavaScript code goes here!
+//add event listener to the like button
+errMessage = document.getElementById('modal');
+errMessage.hidden = true;
+const likeButton = document.createElement('button');
+likeButton.textContent = "Like!";
+const likeSection = document.getElementsByClassName('like');
+const likeHeart = document.getElementsByClassName('like-glyph')[0];
+likeButton.addEventListener("click", likeFoo);
+likeSection[0].appendChild(likeButton);
+
+//make the heart appear and disappear
+function likeFoo (e) {
+    console.log('Click!');
+    mimicServerCall()
+      .then(function(serverMessage){
+        serverResp = serverMessage;
+        console.log(serverResp)
+        if (serverResp === 'Pretend remote server notified of action!') {
+            if (likeHeart.innerText === EMPTY_HEART){
+            likeHeart.innerText = FULL_HEART;
+            likeHeart.style.color = 'red'; 
+             }
+             else if (likeHeart.innerText === FULL_HEART){
+                 likeHeart.innerText = EMPTY_HEART;
+                 }
+                }})
+        .catch(function(error) {
+          const errText = document.getElementById('modal-message');
+          errText.textContent = `${error}`
+          errMessage.hidden = false;
+          setTimeout(()=> errMessage.hidden = true, 3000);
+        })
+      }
 
 
-
-
-//------------------------------------------------------------------------------
-// Don't change the code below: this function mocks the server response
-//------------------------------------------------------------------------------
+// //------------------------------------------------------------------------------
+// // Don't change the code below: this function mocks the server response
+// //------------------------------------------------------------------------------
 
 function mimicServerCall(url="http://mimicServer.example.com", config={}) {
   return new Promise(function(resolve, reject) {
